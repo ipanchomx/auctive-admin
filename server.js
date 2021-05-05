fef4091b3865a527096de830df24fa69f18bfe8a
mauduran <is716122@iteso.mx>
2021-05-05 01:32:19 -0500

    fixed server js

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ app.listen(port, () => {
     console.log('App is running on port ' + port);
 })
 
-app.use(express.static(path.join(__dirname, 'dist', 'auctive-ad,om')));
-
+app.use(express.static(path.join(__dirname, 'dist', 'auctive-admin')));
+app.use(express.json());
+app.get('test', (req, res) => {
+    res.json("WELCOME")
+})
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'auctive-admin', 'index.html'));
 })
